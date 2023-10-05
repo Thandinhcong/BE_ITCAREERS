64bf3cbe07656c2d0ed12d1eb454f4dd3ed9b672
huypq8503 <117892182+huypq8503@users.noreply.github.com>
2023-10-05 10:56:08 +0700

    update table ranks

--- database/migrations/2023_10_05_035505_create_ranks_table.php
+++ database/migrations/2023_10_05_035505_create_ranks_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('academic_ranks', function (Blueprint $table) {
+        Schema::create('ranks', function (Blueprint $table) {
             $table->id();
             $table->string('ranks')->unique()->comment('cấp bậc');
             $table->string('description')->nullable()->comment('mô tả');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('academic_ranks');
+        Schema::dropIfExists('ranks');
     }
 };
