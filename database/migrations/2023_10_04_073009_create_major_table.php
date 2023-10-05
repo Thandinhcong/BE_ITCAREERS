8eb9c3e7c6d34574d29a1498faaaa46e6aa646e8
huyetcongtu4869 <huyetcongtu4869@gmail.com>
2023-10-05 22:17:34 +0700

    fix major

--- database/migrations/2023_10_04_073009_create_major_table.php
+++ database/migrations/2023_10_04_073009_create_major_table.php
@@ -16,26 +16,6 @@ return new class extends Migration
             $table->string('major', 55)->unique();
             $table->timestamps();
             $table->softDeletes();
-
-            $table->string('company_name', 50);
-            $table->string('tax_code', 50);
-            $table->string('address')->nullable();
-            $table->date('founded_in')->comment('ngày thành lập');
-            $table->string('name');
-            $table->string('office');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('phone', 12)->unique();
-            $table->string('map')->nullable();
-            $table->string('logo')->nullable();
-            $table->string('link_web');
-            $table->string('image_paper');
-            $table->string('desc');
-            $table->integer('coin')->default(0);
-            $table->string('token');
-            $table->integer('status')->default(0)->comment('0:pending, 1:active ,2:block');
-            $table->timestamps();
-            $table->softDeletes();
         });
     }
 
