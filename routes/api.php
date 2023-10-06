10358117064c4b2d0188bd20ca665d1b2700fda5
HoangNam16 <namx5bai2003@gmail.com>
2023-10-06 14:09:22 +0700

    skill

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\SkillController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::resource('skill', SkillController::class);
