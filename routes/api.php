f2f22640c8e9eee2c3be3c2ea4a567c61db503f0
HoangNam16 <namx5bai2003@gmail.com>
2023-10-07 17:10:09 +0700

    experience

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ExperienceController;
 use App\Http\Controllers\Admin\SkillController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::resource('skill', SkillController::class);
+Route::resource('experience', ExperienceController::class);
