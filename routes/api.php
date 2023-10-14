3e2e537ab9ad3827f50fb0ca3b5cd6bd8055e1ad
HoangNam16 <namx5bai2003@gmail.com>
2023-10-14 22:20:10 +0700

    experience

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,6 @@
 <?php
 
-<<<<<<< HEAD
-=======
 use App\Http\Controllers\Admin\PackageController;
->>>>>>> 11ee8d7499c0f6bb10e0d9e294677a97daa03c0e
 use App\Http\Controllers\Admin\ExpController;
 use App\Http\Controllers\Admin\ExperienceController;
 use App\Http\Controllers\Admin\SkillController;
