256d6d60810483035dc46fef886b16c415b1563b
thandinhjs <dinhcongthan03@gmail.com>
2023-10-20 21:54:17 +0700

    update

--- config/auth.php
+++ config/auth.php
@@ -54,11 +54,11 @@ return [
         ],
         'company-api' => [
             'driver' => 'passport',
-            'provider' => 'company',
+            'provider' => 'companies',
         ],
         'company' => [
             'driver' => 'session',
-            'provider' => 'company',
+            'provider' => 'companies',
         ],
 
     ],
@@ -93,8 +93,7 @@ return [
             'driver' => 'eloquent',
             'model' => \App\Models\Company::class,
         ],
-
-        // 'users' => [
+// 'users' => [
         //     'driver' => 'database',
         //     'table' => 'users',
         // ],
