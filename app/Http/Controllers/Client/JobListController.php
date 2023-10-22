89978b0fcbea20d58fed9d439d8d29c3e4dd472f
huyetcongtu4869 <huyetcongtu4869@gmail.com>
2023-10-22 16:27:21 +0700

    BE026

--- app/Http/Controllers/Client/JobListController.php
+++ app/Http/Controllers/Client/JobListController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\JobPost;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class JobListController extends Controller
@@ -82,4 +83,33 @@ class JobListController extends Controller
             ], 404);
         }
     }
+    public function job_apply() {
+        $candidate_id = Auth::guard('candidate')->user()->id;
+        $job_apply = DB::table('job_post_apply')
+        ->join('job_post', 'job_post_apply.job_post_id', '=', 'job_post.id')
+        ->join('profile', 'job_post_apply.profile_id', '=', 'profile.id')
+        ->join('companies', 'companies.id', '=', 'job_post.company_id')
+        ->where('profile.candidate_id',$candidate_id)
+        ->select(
+            'job_post.id',
+            'job_post.title',
+            // 'area.area',
+            'job_post.min_salary',
+            'job_post.max_salary',
+            'companies.name as company_name',
+            'companies.logo',
+            'job_post_apply.created_at as time_apply',
+        )->get(); 
+    if ($job_apply) {
+        return response()->json([
+            'status' => 200,
+            'job_list' => $job_apply
+        ], 200);
+    } else {
+        return response()->json([
+            'status' => 404,
+            'mesage' => 'không có bản ghi nào',
+            'job_list' => []
+        ], 404);
+    }    }
 }
