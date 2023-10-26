065e3cf778348c3cde543c8bcdc3ec1391af3482
HoangNam16 <namx5bai2003@gmail.com>
2023-10-26 21:40:54 +0700

    update khuvuc

--- app/Models/JobPost.php
+++ app/Models/JobPost.php
@@ -10,8 +10,8 @@ class JobPost extends Model
     use HasFactory;
     protected $table = 'job_post';
     protected $fillable = [
-        'id', 'title', 'job_position_id', 'exp_id', 'quantity', 'require', 'interest','min_salary','max_salary',
-        'company_id', 'area_id', 'working_form_id', 'academic_level_id',  'major_id','gender',
-        'start_date', 'end_date', 'status'
+        'id', 'title', 'job_position_id', 'exp_id', 'quantity', 'require', 'interest', 'min_salary', 'max_salary',
+        'company_id', 'area_id', 'working_form_id', 'academic_level_id',  'major_id', 'gender',
+        'start_date', 'end_date', 'status', 'district', 'province_id'
     ];
 }
