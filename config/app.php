c8107e568dc5db4f48d3d19b0544a2213a7b1318
huypq8503 <117892182+huypq8503@users.noreply.github.com>
2023-11-02 14:45:25 +0700

    update env

--- config/app.php
+++ config/app.php
@@ -70,7 +70,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => env('APP_TIMEZONE'),
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
